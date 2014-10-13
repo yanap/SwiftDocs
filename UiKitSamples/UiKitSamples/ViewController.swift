b861f61fbfe613047fe28345efc8165d3ac21323
yanap <yanap1214@gmail.com>
2014-10-13 20:33:01 +0900

    add framework

--- UiKitSamples/UiKitSamples/ViewController.swift
+++ UiKitSamples/UiKitSamples/ViewController.swift
@@ -8,28 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    // Tableで使用する配列を設定する
+    let myItems: NSArray = ["TEST1", "TEST2", "TEST3"]
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-    
-        // UIImageViewを作成する
-        let myImageView: UIImageView = UIImageView(frame: CGRectMake(0, 0, 240, 120))
-        
-        // 表示する画像を設定する
-        let myImage = UIImage(named: "logo.png")
-        
-        // 画像をUIImageViewに設定する
-        myImageView.image = myImage
-        
-        // 画像の表示する座標を指定する
-        myImageView.layer.position = CGPoint(x: self.view.bounds.width/2, y:200.0)
-        
-        // UIImageViewをViewに追加する
-        self.view.addSubview(myImageView)
+        <#code#>
     }
     
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        <#code#>
+    }
+    
+    /*
+    Cellが選択された際に呼び出される
+    */
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        <#code#>
+    }
+    
+    /*
+    Cellの総数を返す
+    */
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    /*
+    Cellに値を設定する
+    */
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        <#code#>
     }
 }
