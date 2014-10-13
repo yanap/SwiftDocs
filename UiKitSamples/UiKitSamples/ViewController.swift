069fc1a893b5543bdb6b5f1b87d6e5fe43e6549a
yanap <yanap1214@gmail.com>
2014-10-13 21:23:27 +0900

    add table view
    テーブルを表示

--- UiKitSamples/UiKitSamples/ViewController.swift
+++ UiKitSamples/UiKitSamples/ViewController.swift
@@ -14,31 +14,60 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     let myItems: NSArray = ["TEST1", "TEST2", "TEST3"]
     
     override func viewDidLoad() {
-        <#code#>
+        super.viewDidLoad()
+        
+        // Status Barの高さを取得する
+        let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
+        
+        // Viewの高さと幅を取得する
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        // TableViewの生成する(status barの高さ分ずらして表示)
+        let myTableView: UITableView = UITableView(frame: CGRect(x: 0, y:barHeight, width: displayWidth, height: displayHeight - barHeight))
+        
+        // Cell名の登録を行う
+        myTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        // DataSourceの設定をする
+        myTableView.dataSource = self
+        
+        // Delegateを設定する
+        myTableView.delegate = self
+        
+        // Viewに追加する
+        self.view.addSubview(myTableView)
     }
     
     override func didReceiveMemoryWarning() {
-        <#code#>
+        super.didReceiveMemoryWarning()
     }
     
     /*
     Cellが選択された際に呼び出される
     */
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        <#code#>
+        println("Num: \(indexPath.row)")
+        println("Value: \(myItems[indexPath.row])")
     }
     
     /*
     Cellの総数を返す
     */
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return myItems.count
     }
     
     /*
     Cellに値を設定する
     */
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        <#code#>
+        // Cellを取得する
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath) as UITableViewCell
+        
+        // Cellに値を設定する
+        cell.textLabel?.text = "\(myItems[indexPath.row])"
+        
+        return cell
     }
 }
